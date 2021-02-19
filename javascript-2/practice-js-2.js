f4969c91b9e3207b7097b3b9bc0a4dee63edd0a3
Carlos Chavez <hamandatorres@gmail.com>
2021-02-19 15:24:21 -0700

    javascript 2

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = 'Git is a repository where information(code) is kept to be accessed at a later time by either its author or others'
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = 'Github is a cloud based repository where information(code) can be kept, and where it can copied, accessed, and altered by other users.'
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,7 +34,10 @@
 */
 
 //CODE HERE
-
+const init = {
+    description: 'this command creates the git repository within the open file',
+    code: 'git init',
+}
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,7 +45,10 @@
 */
 
 //CODE HERE
-
+const clone = {
+    description: 'clones a repository from github onto a local machine and into the desired location',
+    code: 'git clone url',
+}
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -50,7 +56,10 @@
 */
 
 //CODE HERE
-
+const status = {
+    description: 'this command lists which files are staged, unstaged, and untrackec',
+    code: 'git status',
+}
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,7 +67,10 @@
 */
 
 //CODE HERE
-
+const add = {
+    description: 'stages all the changes in the directory for the next commit',
+    code: 'git add .',
+}
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +78,10 @@
 */
 
 //CODE HERE
-
+const commit = {
+    description: 'commits the staged changes using a message',
+    code: 'git commit -m',
+}
 //////////////////PROBLEM 8////////////////////
 /*
     Create an object called 'addRemote' with 'description' and 'code' properties
@@ -74,7 +89,10 @@
 */
 
 //CODE HERE
-
+const addRemote = {
+    description: 'creates a new connection to a remote repo.',
+    code: 'git remote add',
+}
 //////////////////PROBLEM 9////////////////////
 /*
     Create an object called 'push' with 'description' and 'code' properties 
@@ -82,3 +100,7 @@
 */
 
 //CODE HERE
+const push = {
+    description: 'pushes the branch to the github repo along with necessary commits and objects',
+    code: 'git push',
+}
